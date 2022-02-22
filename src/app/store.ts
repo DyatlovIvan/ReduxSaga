8fb220015adf0dce6a7df82f4c5627d094bf731d
DyatlovIvan <diatlovivan92@mail.ru>
2022-02-22 14:50:45 +0400

    test

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,7 +4,8 @@ import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
-
+import createSagaMiddleware from 'redux-saga'
+import {takeEvery} from 'redux-saga/effects'
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
@@ -13,11 +14,23 @@ const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer
 })
+
+const sagaMiddleware = createSagaMiddleware()
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,sagaMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
+sagaMiddleware.run(rootWatcher)
+function* rootWatcher(){
+    yield takeEvery('ACTIVATOR-ACTION-TYPE',rootWorker)
+}
+
+function* rootWorker(){
+    alert('1')
+}
+// @ts-ignore
+setTimeout(()=>{store.dispatch({type:'ACTIVATOR-ACTION-TYPE'})},2000)
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
